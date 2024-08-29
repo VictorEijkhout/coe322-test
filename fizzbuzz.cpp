0c6dbe87a51d64bff2b09c9de58277058dd11e88
eijkhout <eijkhout@isp03.tacc.utexas.edu>
2024-08-29 16:19:12 -0500

    first attempt att fizzbuzz

---
+++ fizzbuzz.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+// Nope. The professor does not like this. using namespace std;
+using std::cout;
+using std::endl;
+
+int main() {
+  if ( (1>0) and (2>1) ) 
+    cout << "Hello world\n";
+  //alternatively  cout << "Hello world" << '\n';
+  return 0;
+}
+
+// C-x C-s : save this file
+// C-x C-c : quit the editor, but often better to:
+// C-z : suspend the editor
+// on the unix level: fg brings it back
+
